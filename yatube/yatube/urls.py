64273fa2b1f18279664f9ad4cc7a71d408be03a7
Мансур Миндугулов <mindugulovmansur@gmail.com>
2023-02-12 18:31:54 +0300

    second

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 handler404 = 'core.views.page_not_found'
+handler403 = 'core.views.permission_denied'
 
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
